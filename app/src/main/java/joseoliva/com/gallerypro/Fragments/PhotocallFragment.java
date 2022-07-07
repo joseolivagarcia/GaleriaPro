be26bd19a9152fef9c13559f8182cd5636571a6c
Jose Oliva Local <jose@joseoliva.com>
2022-07-08 00:27:00 +0200

    Añadido contenido Photocall

--- app/src/main/java/joseoliva/com/gallerypro/Fragments/PhotocallFragment.java
+++ app/src/main/java/joseoliva/com/gallerypro/Fragments/PhotocallFragment.java
@@ -3,11 +3,14 @@ package joseoliva.com.gallerypro.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import joseoliva.com.gallerypro.ImageAdapterCasaReal;
+import joseoliva.com.gallerypro.ImageAdapterPhotocall;
 import joseoliva.com.gallerypro.R;
 
 public class PhotocallFragment extends Fragment {
@@ -19,10 +22,13 @@ public class PhotocallFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_photocall, container, false);
+        ImageAdapterPhotocall imageAdapterPhotocall = new ImageAdapterPhotocall(container.getContext());
+        ViewPager imagegalery = mView.findViewById(R.id.image_galery);
+        imagegalery.setAdapter(imageAdapterPhotocall);
         return mView;
+
     }
 }
---
+++ app/src/main/java/joseoliva/com/gallerypro/ImageAdapterPhotocall.java
@@ -0,0 +1,49 @@
+package joseoliva.com.gallerypro;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ImageAdapterPhotocall extends PagerAdapter {
+
+    private TypedArray images;
+    private LayoutInflater layoutInflater;
+
+    public ImageAdapterPhotocall(Context context){
+
+        images = context.getResources().obtainTypedArray(R.array.photocallImages);
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position){
+        View itemView = layoutInflater.inflate(R.layout.element_image, container,false);
+        ImageView image = itemView.findViewById(R.id.item_image);
+        image.setImageDrawable(images.getDrawable(position));
+        ((ViewGroup) image.getParent()).removeView(image);
+        container.addView(image);
+        return image;
+    }
+
+
+    @Override
+    public int getCount() {
+        return images.length();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container,int position, Object object){
+        container.removeView((ImageView) object);
+    }
+}
